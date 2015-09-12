0413eef775b606fdb6a41c26be0bf3b39a5d9e7d
Roessle CircleCI <circleci@roessle.eu>
2015-09-12 19:21:09 +0000

    Updates

--- js/roessle.js
+++ js/roessle.js
@@ -1,7 +1,7 @@
 (function() {
   $(document).ready(function() {
     $('.navigation-colors').before('<div class=navigation-colors-bar></div>');
-    return $('.logo').prepend('<div class=logo-roessle>' + '<div class=logo-hotel-gasthof>Hotel Gasthof</div>' + '<div class=logo-stamp-roessle></div>' + '</div>');
+    return $('.logo').prepend('<a href="/">' + '<div class=logo-roessle>' + '<div class=logo-hotel-gasthof>Hotel Gasthof</div>' + '<div class=logo-stamp-roessle></div>' + '</div>' + '</a>');
   });
 
 }).call(this);
