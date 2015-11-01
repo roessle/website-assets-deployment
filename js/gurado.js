1d536f1b42df3cae9fb242744ec8c22fd078768c
Roessle CircleCI <circleci@roessle.eu>
2015-11-01 14:20:31 +0000

    Updates

--- js/gurado.js
+++ js/gurado.js
@@ -1,21 +1,4 @@
 (function() {
-  (function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function() {
-      (i[r].q = i[r].q || []).push(arguments);
-    };
-    i[r].l = 1 * new Date;
-    a = s.createElement(o);
-    m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m);
-  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-  ga('create', 'UA-3749635-2', 'auto');
-
-  ga('send', 'pageview');
-
   jQuery(document).ready(function() {
     jQuery('.logo').find('img[src$="roessle/media/homepage/websites/2/stamp_roessle.png"]').replaceWith('<div class="visible-desktop visible-tablet logo-stamp-hotel-gasthof-roessle"></div>');
     jQuery('.slider').find('img[src$="roessle//media/subpage/websites/2/home.jpg"]').replaceWith('<div class="span4 hero-1"> <div class="hero-1-txt">Verschenken Sie einen<br>unvergesslichen Augenblick</div> </div> <div class="span8 hero-2"> </div>');
